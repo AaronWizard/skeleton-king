8ba8fe54a753b20ed87ceb072c54268dc4907cbf
AaronWizard <33398493+AaronWizard@users.noreply.github.com>
2025-12-15 13:11:50 -0500

    Reworked how ActorDesign works and is used

--- src/editing/actor_design.gd
+++ src/editing/actor_design.gd
@@ -5,21 +5,34 @@ extends TileObject
 ## A design version of an actor for placing in [MapDesign] scenes while creating
 ## levels.
 
+const _SPRITE_NAME := "Sprite"
+
+
 @export var data: ActorData:
 	set(value):
-		data = value
-		if not is_node_ready():
-			await ready
-
-		_sprite.texture = null
 		if data:
-			_sprite.texture = data.sprite
+			data.changed.disconnect(_update_sprite)
+
+		data = value
+		_update_sprite()
+
+		if data:
+			data.changed.connect(_update_sprite)
 
 
 @export var controller_scene: PackedScene
 
 
 var _sprite: Sprite2D
+
+
+func _get_configuration_warnings() -> PackedStringArray:
+	var result := PackedStringArray()
+	if (get_child_count() == 0) or (get_child(0) is not Sprite2D):
+		result.append("No Sprite2D node")
+	if not data:
+		result.append("No ActorData")
+	return result
 
 
 func _ready() -> void:
@@ -29,11 +42,7 @@ func _ready() -> void:
 			_sprite = sprite
 
 	if not _sprite:
-		_sprite = Sprite2D.new()
-		_sprite.position = pixel_centre
-		add_child(_sprite, false, Node.INTERNAL_MODE_FRONT)
-		_sprite.owner = self
-		move_child(_sprite, 0)
+		_init_sprite()
 
 
 func create_actor() -> Actor:
@@ -52,3 +61,23 @@ func _tile_size_changed() -> void:
 	if not is_node_ready():
 		await ready
 	_sprite.position = pixel_centre
+
+
+func _init_sprite() -> void:
+	_sprite = Sprite2D.new()
+	_sprite.name = _SPRITE_NAME
+	_sprite.position = pixel_centre
+	add_child(_sprite)
+	_sprite.owner = self
+	move_child(_sprite, 0)
+
+	if data:
+		_sprite.texture = data.sprite
+
+
+func _update_sprite() -> void:
+	if not is_node_ready():
+		await ready
+	_sprite.texture = null
+	if data:
+		_sprite.texture = data.sprite
--- src/world/actors/actor_data.gd
+++ src/world/actors/actor_data.gd
@@ -3,7 +3,15 @@ class_name ActorData
 extends Resource
 
 @export var name: String
-@export var sprite: Texture2D
+
+
+@export var sprite: Texture2D:
+	set(value):
+		if sprite != value:
+			sprite = value
+			emit_changed()
+
+
 @export var faction: StringName
 
 @export var base_stats: BaseStats
