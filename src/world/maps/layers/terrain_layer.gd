7b503dc3910cd607f55d5880c07c46625bc6c122
AaronWizard <33398493+AaronWizard@users.noreply.github.com>
2025-12-24 00:23:26 -0500

    Block actors from leaving map
    And cache terrain layer size

--- src/world/maps/layers/terrain_layer.gd
+++ src/world/maps/layers/terrain_layer.gd
@@ -7,23 +7,28 @@ const _TERRAIN_DATA_LAYER_NAME := "terrain"
 @export var terrain_library: TerrainLibrary
 
 
+var _rect := Rect2i(Vector2i.ZERO, Vector2i(-1, -1))
+var _tile_size := Vector2i(-1, -1)
+
 func get_pixel_rect() -> Rect2i:
-	var top_layer := get_children().back() as TileMapLayer
-	var tile_size := top_layer.tile_set.tile_size
+	if _tile_size.x == -1:
+		var top_layer := get_children().back() as TileMapLayer
+		_tile_size = top_layer.tile_set.tile_size
 	var result := get_cell_rect()
-	result.position *= tile_size
-	result.size *= tile_size
+	result.position *= _tile_size
+	result.size *= _tile_size
 	return result
 
 
 func get_cell_rect() -> Rect2i:
-	var result := Rect2i()
-	for layer: TileMapLayer in get_children():
-		var layer_rect := layer.get_used_rect()
-		result = result.merge(
-			Rect2i(layer_rect.position, layer_rect.size)
-		)
-	return result
+	if _rect.size.x == -1:
+		_rect.size = Vector2.ZERO
+		for layer: TileMapLayer in get_children():
+			var layer_rect := layer.get_used_rect()
+			_rect = _rect.merge(
+				Rect2i(layer_rect.position, layer_rect.size)
+			)
+	return _rect
 
 
 func _get_configuration_warnings() -> PackedStringArray:
@@ -46,13 +51,16 @@ func get_terrain(cell: Vector2i) -> Terrain:
 
 
 func actor_can_enter_cell(actor: Actor, cell: Vector2i) -> bool:
-	var result := true
+	if not get_cell_rect().has_point(cell):
+		return false
+
 	for covered_cell in actor.get_covered_cells_at_cell(cell):
 		var terrain_data := get_terrain(covered_cell)
-		result = not terrain_data or not terrain_data.blocks_move
-		if not result:
-			break
-	return result
+		var blocks_move = terrain_data and terrain_data.blocks_move
+		if blocks_move:
+			return false
+
+	return true
 
 
 func _get_terrain_name(cell: Vector2i) -> StringName:
