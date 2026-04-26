8fcb21ed9ce48ebf6985c790637d6b22887509d5
AaronWizard <33398493+AaronWizard@users.noreply.github.com>
2026-04-25 23:17:04 -0400

    Removed BT framework

--- src/world/control/behaviour_tree_controller.gd
+++
@@ -1,8 +0,0 @@
-class_name BehaviourTreeController
-extends ActorController
-
-@export var behaviour_tree: BehaviourTreeNode
-
-
-func get_turn_action() -> TurnAction:
-	return behaviour_tree.get_action(actor)
--- src/world/control/behaviour_trees/bt_node.gd
+++
@@ -1,9 +0,0 @@
-@abstract
-class_name BehaviourTreeNode
-extends Resource
-
-## A node in a behaviour tree.
-
-## Gets a turn action for an actor, or null to make the actor do nothing on its
-## turn.
-@abstract func get_action(actor: Actor) -> TurnAction
