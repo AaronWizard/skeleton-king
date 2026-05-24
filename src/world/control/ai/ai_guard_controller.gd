ce81f604e5fe0e6e13d08eb0a5b5b9c60f748b52
AaronWizard <33398493+AaronWizard@users.noreply.github.com>
2026-05-24 15:02:56 -0400

    AI restructuring
    - Added "investigate" state
    - Switch from "investigate" to "search" when either (1) actor reaches investigation tile or (2) ally reaches investigation tile

--- src/world/control/ai/ai_guard_controller.gd
+++ src/world/control/ai/ai_guard_controller.gd
@@ -5,6 +5,7 @@ enum _State
 {
 	IDLE,
 	CHASE,
+	INVESTIGATE,
 	SEARCH,
 	RETURN
 }
@@ -21,102 +22,43 @@ const _YELL_EVENT_ID := &"guard_saw_enemy_event"
 const _YELL_DATA_ENEMY_LOCATION := &"enemy_location"
 const _YELL_DATA_SOURCE_ACTOR := &"source_actor"
 
+#region State Data
+
 var _initial_cell: Vector2i
+
 var _target_enemy: Actor = null
 
-var _target_last_rect: Rect2i
-var _search_rect: Rect2i
+var _investigate_target: Rect2i
+
+var _search_cell: Vector2i
 var _search_count := 0
 
 var _closest_alert_target: Rect2i
 var _closest_alert_target_dist_sqr := -1.0
 
-var _state := _State.IDLE
+#endregion State Data
 
+#region State Structure
 
 var _state_transitions: Dictionary[_State, Callable] = {
-	_State.IDLE:
-		func () -> _State:
-			if _target_enemy:
-				_yell_for_help(_target_last_rect)
-				return _State.CHASE
-
-			if _process_alert_target():
-				return _State.SEARCH
-
-			return _State.IDLE,
-
-	_State.CHASE:
-		func () -> _State:
-			if _target_enemy:
-				return _State.CHASE
-
-			_search_rect = _target_last_rect
-			_search_count = _MAX_SEARCH_COUNT
-			return _State.SEARCH,
-
-	_State.SEARCH:
-		func () -> _State:
-			if _target_enemy:
-				_yell_for_help(_target_last_rect)
-				return _State.CHASE
-
-			if _process_alert_target():
-				return _State.SEARCH
-
-			if actor.cell_rect.intersects(_search_rect):
-				_search_count -= 1
-			if _search_count == 0:
-				return _State.RETURN
-
-			return _State.SEARCH,
-
-	_State.RETURN:
-		func () -> _State:
-			if _target_enemy:
-				_yell_for_help(_target_last_rect)
-				return _State.CHASE
-
-			if _process_alert_target():
-				return _State.SEARCH
-
-			if actor.origin_cell == _initial_cell:
-				return _State.IDLE
-
-			return _State.RETURN
+	_State.IDLE: _state_check_idle,
+	_State.CHASE: _state_check_chase,
+	_State.INVESTIGATE: _state_check_investigate,
+	_State.SEARCH: _state_check_search,
+	_State.RETURN: _state_check_return
 }
-
 
 var _state_actions: Dictionary[_State, Callable] = {
-	_State.IDLE:
-		func () -> TurnAction:
-			return null,
-
-	_State.CHASE:
-		func () -> TurnAction:
-			if TileGeometry.rects_are_adjacent(
-					actor.cell_rect, _target_enemy.cell_rect):
-				return AttackAction.new(actor, _target_enemy)
-			else:
-				return _head_to_rect(_target_enemy.cell_rect),
-
-	_State.SEARCH:
-		func () -> TurnAction:
-			if actor.cell_rect.intersects(_search_rect):
-				@warning_ignore("integer_division")
-				return _pick_new_search_rect(
-					_target_last_rect.position + (_target_last_rect.size / 2)
-				)
-			else:
-				return _head_to_rect(_search_rect),
-
-	_State.RETURN:
-		func () -> TurnAction:
-			var path := ActorPathfinder.find_path_to_cell(actor, _initial_cell, false)
-			if not path.is_empty():
-				return MoveAction.new(actor, path[0])
-			return null
+	_State.IDLE: _state_action_idle,
+	_State.CHASE: _state_action_chase,
+	_State.INVESTIGATE: _state_action_investigate,
+	_State.SEARCH: _state_action_search,
+	_State.RETURN: _state_action_return
 }
+
+#endregion State Structure
+
+var _state := _State.IDLE
 
 
 func _ready() -> void:
@@ -127,21 +69,139 @@ func _ready() -> void:
 
 
 func get_turn_action() -> TurnAction:
-	_check_for_enemy()
 	_state = _state_transitions[_state].call()
 	return _state_actions[_state].call()
+
+
+#region State Checks
+
+func _state_check_idle() -> _State:
+	_check_for_enemy()
+	if _target_enemy:
+		_yell_for_help(_investigate_target)
+		return _State.CHASE
+
+	if _will_investigate_alert_target():
+		return _State.INVESTIGATE
+
+	return _State.IDLE
+
+
+func _state_check_chase() -> _State:
+	_check_for_enemy()
+	if _target_enemy:
+		return _State.CHASE
+	else:
+		_search_count = _MAX_SEARCH_COUNT
+		return _State.INVESTIGATE
+
+
+func _state_check_investigate() -> _State:
+	_check_for_enemy()
+	if _target_enemy:
+		_yell_for_help(_investigate_target)
+		return _State.CHASE
+
+	# Investigate closer target when alerted about one
+	_will_investigate_alert_target()
+
+	var ally_at_target := func (other_actor: Actor) -> bool:
+		return (actor != other_actor) \
+				and not Actor.are_enemies(actor, other_actor)
+
+	if actor.cell_rect.intersects(_investigate_target) \
+			or ( \
+				_can_see_rect(_investigate_target) \
+				and actor.map.get_actors_in_rect(_investigate_target).any( \
+					ally_at_target \
+				) \
+			):
+		if _pick_new_search_cell():
+			return _State.SEARCH
+		else:
+			_search_count = 0
+			return _State.RETURN
+
+	return _State.INVESTIGATE
+
+
+func _state_check_search() -> _State:
+	_check_for_enemy()
+	if _target_enemy:
+		_yell_for_help(_investigate_target)
+		return _State.CHASE
+
+	if _will_investigate_alert_target():
+		return _State.INVESTIGATE
+
+	if actor.origin_cell == _search_cell:
+		_search_count -= 1
+		if (_search_count == 0) or not _pick_new_search_cell():
+			_search_count = 0
+			return _State.RETURN
+
+	return _State.SEARCH
+
+
+func _state_check_return() -> _State:
+	_check_for_enemy()
+	if _target_enemy:
+		_yell_for_help(_investigate_target)
+		return _State.CHASE
+
+	if _will_investigate_alert_target():
+		return _State.INVESTIGATE
+
+	if actor.origin_cell == _initial_cell:
+		return _State.IDLE
+	else:
+		return _State.RETURN
+
+#endregion State Checks
+
+#region State Actions
+
+func _state_action_idle() -> TurnAction:
+	return null
+
+
+func _state_action_chase() -> TurnAction:
+	if TileGeometry.rects_are_adjacent(
+			actor.cell_rect, _target_enemy.cell_rect):
+		return AttackAction.new(actor, _target_enemy)
+	else:
+		return _head_to_rect(_target_enemy.cell_rect)
+
+
+func _state_action_investigate() -> TurnAction:
+	return _head_to_rect(_investigate_target)
+
+
+func _state_action_search() -> TurnAction:
+	return _head_to_cell(_search_cell)
+
+
+func _state_action_return() -> TurnAction:
+	return _head_to_cell(_initial_cell)
+
+
+#endregion State Actions
 
 
 func _check_for_enemy() -> void:
 	var enemy := WorldQueries.get_closest_enemy(actor)
 	if enemy and _can_see_actor(enemy):
 		_target_enemy = enemy
-		_target_last_rect = _target_enemy.cell_rect
+		_investigate_target = _target_enemy.cell_rect
 	else:
 		_target_enemy = null
 
 
 func _can_see_actor(other_actor: Actor) -> bool:
+	return _can_see_rect(other_actor.cell_rect)
+
+
+func _can_see_rect(rect: Rect2i) -> bool:
 	var sight_rect := Rect2i(
 		-_SIGHT_RANGE, -_SIGHT_RANGE,
 		_SIGHT_RANGE * 2, _SIGHT_RANGE * 2
@@ -149,17 +209,20 @@ func _can_see_actor(other_actor: Actor) -> bool:
 	sight_rect.position += actor.origin_cell
 	sight_rect.size += actor.cell_size
 
-	return sight_rect.intersects(other_actor.cell_rect)
+	return sight_rect.intersects(rect)
 
 
-func _pick_new_search_rect(center_cell: Vector2i) -> TurnAction:
-	var result: TurnAction = null
+func _pick_new_search_cell() -> bool:
+	var result := false
 
 	var wander_rect := Rect2i(
 		-_SEARCH_RANGE, -_SEARCH_RANGE,
 		_SEARCH_RANGE * 2, _SEARCH_RANGE * 2
 	)
-	wander_rect.position += center_cell
+
+	@warning_ignore("integer_division")
+	wander_rect.position += \
+			_investigate_target.position + (_investigate_target.size / 2)
 	wander_rect.size += actor.cell_size
 
 	var cells := TileGeometry.cells_in_rect(wander_rect)
@@ -169,8 +232,8 @@ func _pick_new_search_rect(center_cell: Vector2i) -> TurnAction:
 		if actor.map.actor_can_enter_cell(actor, target, true):
 			var path := ActorPathfinder.find_path_to_cell(actor, target, true)
 			if not path.is_empty():
-				_search_rect = Rect2i(target, Vector2i.ONE)
-				result = MoveAction.new(actor, path[0])
+				_search_cell = target
+				result = true
 				break
 
 	return result
@@ -184,6 +247,13 @@ func _head_to_rect(rect: Rect2i) -> TurnAction:
 	return result
 
 
+func _head_to_cell(cell: Vector2i) -> TurnAction:
+	var path := ActorPathfinder.find_path_to_cell(actor, cell, false)
+	if not path.is_empty():
+		return MoveAction.new(actor, path[0])
+	return null
+
+
 func _yell_for_help(enemy_rect: Rect2i) -> void:
 	actor.map.events.send_custom_event(
 		_YELL_EVENT_ID,
@@ -195,18 +265,25 @@ func _yell_for_help(enemy_rect: Rect2i) -> void:
 	)
 
 
-func _process_alert_target() -> bool:
+func _will_investigate_alert_target() -> bool:
 	if _closest_alert_target_dist_sqr < 0:
+		# We've not been alerted
 		return false
 
-	_search_rect = _closest_alert_target
+	if _state == _State.INVESTIGATE:
+		var curr_invst_rect_dist_sqr := TileGeometry.rect_distance_squared(
+				actor.cell_rect, _investigate_target)
+		if _closest_alert_target_dist_sqr > curr_invst_rect_dist_sqr:
+			# Alert target farther than what we're already investigating
+			return false
 
+	_investigate_target = _closest_alert_target
+
+	# Reset closest alert tracking
 	_closest_alert_target = Rect2i()
 	_closest_alert_target_dist_sqr = -1.0
 
-	if _search_count == 0:
-		_search_count = 1
-
+	# Alerted about rect to investigate
 	return true
 
 
