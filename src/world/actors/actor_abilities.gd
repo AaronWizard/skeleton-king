2f4c7776ba2d9ef030aadd0042a1b571807c06b8
AaronWizard <33398493+AaronWizard@users.noreply.github.com>
2026-05-26 01:27:50 -0400

    Added ActorAbilities

---
+++ src/world/actors/actor_abilities.gd
@@ -0,0 +1,23 @@
+class_name ActorAbilities
+
+const _ATTACK_ABILITY := preload("uid://dr0s6kvi2j80t") as Ability
+
+
+var _actor: Actor
+
+
+func _init(p_actor: Actor) -> void:
+	_actor = p_actor
+
+
+var attack: Ability:
+	get:
+		return _ATTACK_ABILITY
+
+
+func can_attack(target: Vector2i) -> bool:
+	return attack.target_valid(_actor, target)
+
+
+func create_attack_action(target: Vector2i) -> TurnAction:
+	return attack.create_turn_action(_actor, target)
